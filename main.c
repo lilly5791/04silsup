f7a98b1730ad2f4bb1c44a1a77512204e9a98e99
lilly5791 <55526659+lilly5791@users.noreply.github.com>
2019-10-01 16:03:01 +0900

    Update main.c

--- main.c
+++ main.c
@@ -21,38 +21,3 @@ int main(int argc, char *argv[]) {
 	
 	return 0;
 }
-
-int main(int argc, char *argv[]) {
-	
-	int x = 3;
-	int cond1, cond2; // silsup 4week 2
-	
-	cond1 = 2 < x && x < 5;
-	cond2 = x > 1 || x < 4 && x > 3;
-	
-	printf("cond1 = %d , cond2 = %d\n", cond1, cond2);
-	
-	return 0;
-}
-
-int main(int argc, char *argv[]) {
-	
-	int x, y, a, b, c, d, e; // silsup 4week 3
-	
-	printf("Input two integers : ");
-	scanf("%d, %d", &x, &y);
-	
-	a = x + y;
-	b = x - y;
-	c = x * y;
-	d = x / y;
-	e = x % y;
-	
-	printf("+ result is : %d\n", a);
-	printf("- result is : %d\n", b);
-	printf("* result is : %d\n", c);
-	printf("/ result is : %d\n", d);
-	printf("%% result is : %d\n", e);
-	
-	return 0;
-}
